848644d63c555fa4cd2d30a009a9fd058a7b6b03
Hannes Carlsson <hannescarlsson42@gmail.com>
2020-12-14 08:40:54 +0100

    apply mask correctly

--- 2020/day14/src/main.rs
+++ 2020/day14/src/main.rs
@@ -10,27 +10,28 @@ fn main() {
         .collect();
     for block in writes{
         let mask = &block.iter().next().unwrap()[7..];
-        let or_mask_str = mask.chars().clone().map(|c| match c {
-            'X'=>'1',
-            _ => '0'
-        }).collect::<String>();
-        let or_mask = u64::from_str_radix(&or_mask_str,2).unwrap();
-        let and_mask_str = mask.chars().clone().map(|c| match c {
+        let one_mask_str = mask.chars().clone().map(|c| match c {
             '1'=>'1',
             _ => '0'
         }).collect::<String>();
-        let and_mask = u64::from_str_radix(&and_mask_str,2).unwrap();
+        let one_mask = u64::from_str_radix(&one_mask_str,2).unwrap();
+        let zero_mask_str = mask.chars().clone().map(|c| match c {
+            '0'=>'0',
+            _ => '1'
+        }).collect::<String>();
+        let zero_mask = u64::from_str_radix(&zero_mask_str,2).unwrap();
         for &written_line in block.iter().skip(1) {
             let mut line_iter =written_line.split('=');
             let address = line_iter.next().unwrap().trim().parse().unwrap();
             let value = line_iter.next().unwrap().trim().parse().unwrap();
-            memory.insert(address,apply_mask(and_mask,or_mask,value));
+            let mask_val =apply_mask(zero_mask,one_mask,value);
+            memory.insert(address,mask_val);
         }
         
     }
     let mem_sum:u64 =memory.values().sum();
     println!("mem_sum: {}",mem_sum)
 }
-fn apply_mask(and_m:u64,or_m:u64, val:u64)->u64{
-    (val & and_m) | (val|or_m)
+fn apply_mask(zero_m:u64,one_m:u64, val:u64)->u64{
+    (val & zero_m) |one_m
 }
